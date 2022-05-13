de648c621cceeae6edfc09e1141e6e504d86c2b0
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2022-05-13 17:44:48 +0300

    footer, reviews, location, contacts

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,16 @@
 $(function() {
-  
+    $('.reviews__slider').slick({
+        infinite: true,
+        arrows: false,
+        dots:true
+      });
+    $('.viewAll__link').on('click', function(event) {
+      event.preventDefault()
+    })
+
+    var mixer = mixitup('.location__items-box', {
+      load: {
+        filter: '.fiveStar'
+      }
+    });
 })
