285f9c3202bc6fe31ad21b646c26d7b7e1a01f24
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2022-05-14 20:19:31 +0300

    burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,46 @@ $(function() {
       });
     $('.viewAll__link').on('click', function(event) {
       event.preventDefault()
-    })
+    });
 
     var mixer = mixitup('.location__items-box', {
       load: {
         filter: '.fiveStar'
       }
     });
+
+    $('.header__burger').on('click', function(event) {
+      event.preventDefault();
+      $('.header__menu').toggleClass('open');
+    });
+
+    $("#calendar").datepicker();
 })
+
+const menuLinks = document.querySelectorAll('.header__menu-link[data-scroll]');
+
+if(menuLinks.length > 0) {
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', onMenuLinkClick);
+  });
+
+  function onMenuLinkClick(e) {
+    const menuLink = e.target;
+    if(menuLink.dataset.scroll && document.querySelector(menuLink.dataset.scroll)) {
+      const scrollToBlock = document.querySelector(menuLink.dataset.scroll);
+      const scrollToBlockValue = scrollToBlock.getBoundingClientRect().top + scrollY - document.querySelector('.header').offsetHeight;
+      e.preventDefault();
+
+      window.scrollTo({
+        top: scrollToBlockValue,
+        behavior: 'smooth'
+      });
+
+      document.querySelector('.header__menu').classList.remove('open');
+    }
+  }
+}
+
+new AirDatepicker('#calendar', {
+  selectedDates: [new Date()]
+});
