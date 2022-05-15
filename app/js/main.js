b1ec9ca1aa692e6beb6b36341781310816f3a4df
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2022-05-15 20:35:29 +0300

    dropdowns

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,36 @@
-$(function() {
-    $('.reviews__slider').slick({
-        infinite: true,
-        arrows: false,
-        dots:true
-      });
-    $('.viewAll__link').on('click', function(event) {
-      event.preventDefault()
-    });
+$(function () {
+  $('.reviews__slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: true
+  });
+  $('.viewAll__link').on('click', function (event) {
+    event.preventDefault()
+  });
 
-    var mixer = mixitup('.location__items-box', {
-      load: {
-        filter: '.fiveStar'
-      }
-    });
+  var mixer = mixitup('.location__items-box', {
+    load: {
+      filter: '.fiveStar'
+    }
+  });
 
-    $('.header__burger').on('click', function(event) {
-      event.preventDefault();
-      $('.header__menu').toggleClass('open');
-    });
-
-    $("#calendar").datepicker();
+  $('.header__burger').on('click', function (event) {
+    event.preventDefault();
+    $('.header__menu').toggleClass('open');
+  });
 })
 
+// scroll
 const menuLinks = document.querySelectorAll('.header__menu-link[data-scroll]');
 
-if(menuLinks.length > 0) {
+if (menuLinks.length > 0) {
   menuLinks.forEach(menuLink => {
     menuLink.addEventListener('click', onMenuLinkClick);
   });
 
   function onMenuLinkClick(e) {
     const menuLink = e.target;
-    if(menuLink.dataset.scroll && document.querySelector(menuLink.dataset.scroll)) {
+    if (menuLink.dataset.scroll && document.querySelector(menuLink.dataset.scroll)) {
       const scrollToBlock = document.querySelector(menuLink.dataset.scroll);
       const scrollToBlockValue = scrollToBlock.getBoundingClientRect().top + scrollY - document.querySelector('.header').offsetHeight;
       e.preventDefault();
@@ -46,6 +45,52 @@ if(menuLinks.length > 0) {
   }
 }
 
+// calendar
 new AirDatepicker('#calendar', {
-  selectedDates: [new Date()]
+  selectedDates: [new Date()],
+  minDate: [new Date()],
+  autoClose: true
+});
+
+// select guests list
+document.querySelectorAll('.booking__form-select').forEach(function (selectWrapper) {
+  const openBtn = selectWrapper.querySelector('.booking__select-btn');
+  const openList = selectWrapper.querySelector('.booking__form-list');
+  const openItems = openList.querySelectorAll('.booking__form-item');
+  const hiddenInput = selectWrapper.querySelector('.booking__hidden-input');
+
+  // select list open
+  openBtn.addEventListener('click', function () {
+    openList.classList.toggle('open');
+  });
+  // select lict close
+  openItems.forEach(function (formItem) {
+    formItem.addEventListener('click', function () {
+      openBtn.innerText = this.innerText;
+      openList.classList.remove('open');
+      hiddenInput.value = this.dataset.value;
+    });
+  });
+  // select list close outside
+  document.addEventListener('click', function (e) {
+    if (e.target != openBtn) {
+      openList.classList.remove('open');
+    }
+  });
+});
+
+// select country list
+
+const countryList = document.querySelector('.booking__select-country');
+
+fetch('https://restcountries.com/v3.1/all').then(res => {
+  return res.json();
+}).then(data => {
+  let output = '';
+  data.forEach(country => {
+    output += `<option class="booking__form-item" value="${country.name.common}">${country.name.common}</option>`;
+    countryList.innerHTML = output;
+  });
+}).catch(err => {
+  console.log(err);
 });
